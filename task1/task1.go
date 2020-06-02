d59bd2f6da141f535bd10bb9ed6990db70977227
Vizzzka <usachevavaika171@gmail.com>
2020-06-02 07:17:02 +0300

    fix style

--- task1/task1.go
+++ task1/task1.go
@@ -2,25 +2,25 @@ package task1
 
 import (
 	"encoding/json"
-"fmt"
-"io/ioutil"
-"log"
-"net/http"
-"time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
 const (
-	layoutISO = "2006-01-02"
-	layoutUS  = "January 2, 2006"
+	layoutISO      = "2006-01-02"
+	layoutUS       = "January 2, 2006"
 	nextHolidayURL = "https://date.nager.at/Api/v2/NextPublicHolidays/UA"
 )
 
 type holiday struct {
-	Date string
+	Date      string
 	LocalName string
 }
 
-func isHoliday(date time.Time) bool {
+func isTodayHoliday(date time.Time) bool {
 	currentDate := time.Now().Format(layoutISO)
 	holidayDate := date.Format(layoutISO)
 	return currentDate == holidayDate
@@ -41,7 +41,7 @@ func stringToDate(date string) time.Time {
 	return t
 }
 
-func getNextHoliday () holiday {
+func getNextHoliday() holiday {
 	// get next holidays in UA
 	resp, err := http.Get(nextHolidayURL)
 	if err != nil {
@@ -57,7 +57,7 @@ func getNextHoliday () holiday {
 	// get
 	keys := make([]holiday, 0)
 	json.Unmarshal(body, &keys)
-	return  keys[0]		// return closest holiday
+	return keys[0] // return closest holiday
 }
 
 func Work() {
@@ -65,7 +65,7 @@ func Work() {
 	nextHoliday := getNextHoliday()
 	nextHolidayDate, nextHolidayName := stringToDate(nextHoliday.Date), nextHoliday.LocalName
 
-	if isHoliday(nextHolidayDate) {
+	if isTodayHoliday(nextHolidayDate) {
 		result = fmt.Sprintf("Today holiday is %s, %s.",
 			nextHolidayName, nextHolidayDate.Format(layoutUS))
 	} else {
